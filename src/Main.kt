535542fd97d1d3c14c250e00b8fcecbc1f18a9d6
gonca <goncalolopesta@gmail.com>
2024-03-29 19:09:50 +0000

    all commit

--- src/Main.kt
+++ src/Main.kt
@@ -2,4 +2,18 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
 
-    }
+
+    val entity1 = Entity("curso",null, "Mestrado em Engenharia Informatica")
+    val entity2 = Entity("ano")
+
+
+    val xml = XMLDocument()
+
+    xml.addEntity(entity1)
+    xml.addEntity(entity2)
+
+    xml.removeEntity(entity2)
+
+    print(xml.prettyPrint())
+
+}
--- src/xmlFile.kt
+++ src/xmlFile.kt
@@ -1,9 +1,36 @@
 
-class XMLElement (val entity : Entity){
+class XMLDocument(){
+
+    val entities = mutableListOf<Entity>()
+    fun prettyPrint() : String {
+        var pretty : String? = ""
+
+        for(element in entities){
+            pretty += "<" + element.name + ">" + element.text + "</" + element.name + ">" +  "\n"
+        }
+
+        return pretty.toString();
+    }
+
+    fun addEntity(entity: Entity){
+        entities.add(entity)
+    }
+
+    fun removeEntity(entity: Entity){
+        entities.remove(entity)
+    }
+
 }
 
- sealed interface Entity {
-     val name : String
-     val parent: Entity?
+sealed interface  XMLElement {
+    val name: String
+    val parent: Entity?
+}
+
+ data class Entity(
+     override val name: String,
+     override val parent: Entity? = null,
+     val text : String? = ""
+ ) : XMLElement {
 }
 
